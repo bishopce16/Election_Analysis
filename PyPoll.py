4c47adfd5edbb954bf5d95fb0dd9211fdaf2abf7
Caitlin Bishop <caitlinbishop@Caitlins-MBP.lan>
2022-06-27 15:07:19 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -8,11 +8,13 @@
 # Import the datetime class from the datetime module.
 import datetime as dt
 
-# Assign a variable for the file to load and the path.
-file_to_load = 'Resources/election_results.csv'
 # Add our dependencies.
 import csv
 import os
+
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources/election_results.csv'
+
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
@@ -27,6 +29,13 @@ candidate_options = []
 # Declare the empty dictionary.
 candidate_votes = {}
 
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+
+winning_count = 0
+
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -35,7 +44,7 @@ with open(file_to_load) as election_data:
     
     # Print the header row.
     headers = next(file_reader)
-    print(headers)
+    
     # Print each row in the CSV file.
     for row in file_reader:
         # Add to the total vote count.
@@ -58,13 +67,30 @@ with open(file_to_load) as election_data:
         
         # Determine the percentage of votes for each candidate by looping through the counts.
         # 1. Iterate through the candidate list.
-        for candidate_name in candidate_votes:
-            # 2. Retrieve vote count of a candidate.
-            votes = candidate_votes[candidate_name]
-            # 3. Calculate the percentage of votes.
-            vote_percentage = float(votes) / float(total_votes) * 100
-            # 4. Print the candidate name and percentage of votes.
-            print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes.
+    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+    # Determine winning vote count and candidate
+    
+    # 1. Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # 2. If true then set winning_count = votes and winning_percent =
+        # vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # 3. Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
 
-# Print the candidate vote dictionary.
-print(candidate_votes)
+# Print the winning candidates' results to the terminal.
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+
+print(winning_candidate_summary)
