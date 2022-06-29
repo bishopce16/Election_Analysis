78de0533d56c08283d4ee6834b8283666257649f
Caitlin Bishop <caitlinbishop@Caitlins-MacBook-Pro.local>
2022-06-29 16:57:33 -0400

    Updating PyPoll challenge and readme

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -10,7 +10,7 @@ file_to_load = 'Resources/election_results.csv'
 # Add a variable to load a file from a path.
 file_to_load = os.path.join(".", "Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join(".", "analysis", "election_analysis.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
@@ -20,8 +20,8 @@ candidate_options = []
 candidate_votes = {}
 
 # 1: Create a county list and county votes dictionary.
-
-
+county_names = []
+county_votes = {}
 
 # Track the winning candidate, vote count and percentage
 winning_candidate = ""
@@ -29,8 +29,8 @@ winning_count = 0
 winning_percentage = 0
 
 # 2: Track the largest county and county voter turnout.
-
-
+largest_county_turnout = ""
+largest_county_vote = 0
 
 # Read the csv and convert it into a list of dictionaries
 with open(file_to_load) as election_data:
@@ -49,7 +49,7 @@ with open(file_to_load) as election_data:
         candidate_name = row[2]
 
         # 3: Extract the county name from each row.
-
+        county_name = row[1]
 
         # If the candidate does not match any existing candidate add it to
         # the candidate list
@@ -66,17 +66,17 @@ with open(file_to_load) as election_data:
 
         # 4a: Write an if statement that checks that the
         # county does not match any existing county in the county list.
-
+        if county_name not in county_names:
 
             # 4b: Add the existing county to the list of counties.
-
+            county_names.append(county_name)
 
             # 4c: Begin tracking the county's vote count.
-
+            county_votes[county_name] = 0
 
         # 5: Add a vote to that county's vote count.
-
-
+        county_votes[county_name] += 1
+# iterated over all row in cvs files
 
 # Save the results to our text file.
 with open(file_to_save, "w") as txt_file:
@@ -93,13 +93,19 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a for loop to get the county from the county dictionary.
+    for county in county_votes:
 
         # 6b: Retrieve the county vote count.
-
+        county_vote = county_votes[county]
         # 6c: Calculate the percentage of votes for the county.
-
+        county_percentage = float(county_vote) / float(total_votes) * 100
 
          # 6d: Print the county results to the terminal.
+        county_results = (
+            f"{county}: {county_percentage:.1f}%: ({county_vote})"
+        )
+
+        print(county_results)
 
          # 6e: Save the county votes to a text file.
 
